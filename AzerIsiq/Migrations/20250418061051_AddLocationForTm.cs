23789d6ba3af4d52ad09f1e53476fbc43027d171
“FeryazHacimuradov” <tu201906125@code.edu.az>
2025-04-19 11:01:18 +0400

    UpdatedTmTableAddedLocation

---
+++ AzerIsiq/Migrations/20250418061051_AddLocationForTm.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AzerIsiq.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocationForTm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tms_Locations_LocationId",
+                table: "Tms");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tms_Locations_LocationId",
+                table: "Tms",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tms_Locations_LocationId",
+                table: "Tms");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tms_Locations_LocationId",
+                table: "Tms",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+    }
+}
